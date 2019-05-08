f57d5b22cd1d837a390ccbf8548cd3ddc76a635e
geehye <ghkang93@gmail.com>
2019-05-09 01:51:44 +0900

    first day

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import axios from 'axios'
+import BootstrapVue from 'bootstrap-vue'
+import App from './App'
+import router from './router'
+import less from 'less'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import './assets/calendarStyle.less'
+
+Vue.config.productionTip = false
+Vue.prototype.$http = axios
+Vue.use(BootstrapVue)
+Vue.use(less)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
